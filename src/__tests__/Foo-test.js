acc80cc34f1a868a92989d8df81c1cf1cc5a20d8
Antti Hätinen <antti.hatinen@phz.fi>
2022-05-30 21:52:54 +0300

    Updated to React 16

--- src/__tests__/Foo-test.js
+++ src/__tests__/Foo-test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
+configure({ adapter: new Adapter() });
 
 import Foo from '../Foo';
 
